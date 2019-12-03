1d4ae88fa5e97254831ad73672e8b179d8a6fe0b
debrahrwyatt@gmail.com <debrahrwyatt@gmail.com>
2019-12-02 22:38:19 -0800

    Added some complexity to the hello world file. No neural network today, just more c# basics.

--- NeuralNetwork/Program.cs
+++ NeuralNetwork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace NeuralNetwork
 {
@@ -6,7 +7,47 @@ namespace NeuralNetwork
     {
         static void Main(string[] args)
         {
+            //Basic Syntax
             Console.WriteLine("Hello World!");
+            int a = 1;
+            Console.WriteLine(a);
+
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            string [] temp = Read("WriteLines.txt");
+
+            Write("test.csv", temp);
+
         }
+
+        //The write file function
+        public static void Write(string filename, string [] content){
+            System.IO.File.WriteAllLines(@filename, content);
+        }  
+        
+        //The read file function
+        public static string [] Read(string filename){
+            
+            string [] content = { };
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("WriteLines.txt"))
+                {
+                    string line;
+                    int a = 0;
+                    while ((line = sr.ReadLine()) != null){
+                        content[a++] = line;
+                    }
+                }
+            }
+            catch (Exception e){
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return content;
+        }
+
     }
 }
